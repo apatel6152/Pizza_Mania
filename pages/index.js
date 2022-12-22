de497daec9a8fa50f949e7d960217a3e375b9588
Amit Patel <apatel6152@conestogac.on.ca>
2022-12-21 21:27:58 -0500

    Creating UI for pizza list

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Featured from '../components/Featured'
+import PizzaList from '../components/PizzaList'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -16,6 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Featured />
+      <PizzaList />
     </>
   )
 }
