942bb03d1a27d8da76b3f6f7200d5340ef6ae2f0
Amit Patel <apatel6152@conestogac.on.ca>
2022-12-22 16:45:07 -0500

    Displaying user orders on cart page by using redux

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,10 @@
 import Image from 'next/image';
 import styles from '../styles/Navbar.module.css';
+import { useSelector } from "react-redux";
+import Link from "next/link";
 
 const Navbar = () => {
+  const quantity = useSelector((state) => state.cart.quantity);
   return (
     <div className={styles.container}>
       <div className={styles.item}>
@@ -29,12 +32,14 @@ const Navbar = () => {
         </ul>
       </div>
       
-      <div className={styles.item}>
-        <div className={styles.cart}>
-          <Image src="/img/cart.png" alt="" width="30" height="30" />
-          <div className={styles.counter}>2</div>
+      <Link href="/cart" passHref>
+        <div className={styles.item}>
+          <div className={styles.cart}>
+            <Image src="/img/cart.png" alt="" width="30" height="30" />
+            <div className={styles.counter}>{quantity}</div>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
--- pages/product/[id].js
+++ pages/product/[id].js
@@ -2,12 +2,15 @@ import styles from '../../styles/Product.module.css';
 import Image from 'next/image';
 import { useState } from 'react';
 import axios from 'axios';
+import { useDispatch } from "react-redux";
+import { addProduct } from "../../redux/cartSlice";
 
 const Product = ({ pizza }) => {
   const [size, setSize] = useState(0);
   const [price, setPrice] = useState(pizza.prices[0]);
   const [extras, setExtras] = useState([]);
   const [quantity, setQuantity] = useState(1);
+  const dispatch = useDispatch();
 
   const changePrice = (number) => {
     setPrice(price + number);
@@ -29,6 +32,10 @@ const Product = ({ pizza }) => {
       changePrice(-option.price);
       setExtras(extras.filter((extra) => extra._id !== option._id));
     }
+  };
+
+  const handleClick = () => {
+    dispatch(addProduct({...pizza, extras, price, quantity}));
   };
 
   return (
@@ -79,7 +86,7 @@ const Product = ({ pizza }) => {
             className={styles.quantity}
             onChange={(e) => setQuantity(e.target.value)}
           />
-          <button className={styles.button}>Add to Cart</button>
+          <button className={styles.button} onClick={handleClick}>Add to Cart</button>
         </div>
       </div>
     </div>
