dde9b9390079af697db759fc59e2161c9b2b2d98
Amit Patel <apatel6152@conestogac.on.ca>
2022-12-23 19:00:15 -0500

    adding product and updating order status by admin

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { useState } from "react";
 import Featured from '../components/Featured'
 import PizzaList from '../components/PizzaList'
+import AddButton from "../components/AddButton";
+import Add from "../components/Add";
 
 
-export default function Home({ pizzaList }) {
+export default function Home({ pizzaList, admin }) {
+  const [close, setClose] = useState(true);
   return (
     <>
       <Head>
@@ -16,18 +19,28 @@ export default function Home({ pizzaList }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Featured />
+      {admin && <AddButton setClose={setClose} />}
       <PizzaList pizzaList={pizzaList}/>
+      {!close && <Add setClose={setClose} />}
     </>
   )
 }
 
 export const getServerSideProps = async (ctx) => {
+
+  const myCookie = ctx.req?.cookies || "";
+  let admin = false;
+
+  if (myCookie.token === process.env.TOKEN) {
+    admin = true;
+  }
   
   const res = await axios.get("http://localhost:3000/api/products");
   // console.log(res.data);
   return {
     props: {
       pizzaList: res.data,
+      admin
     },
   };
 };
